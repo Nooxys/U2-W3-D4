d747d29a10835065874ec821913986373473574c
Ciro Vitiello <ciro.riccione@gmail.com>
2024-01-18 23:38:44 +0100

    no extra

--- pexels-start.js
+++ pexels-start.js
@@ -1,19 +1,21 @@
-const firstURL = 'https://api.pexels.com/v1/search?query=moon'
-const secondURL = 'https://api.pexels.com/v1/search?query=wolf'
-const cardImg = document.getElementsByClassName('card-img-top')
+const cardImg = document.querySelectorAll('.card-img-top')
+const allEditButtons = document.querySelectorAll(
+  '.btn-group .btn-sm:nth-of-type(2)'
+)
+const inputField = document.getElementById('custom-search-input')
+const buttonField = document.getElementById('custom-search-button')
+const all9mins = document.querySelectorAll('small')
+console.log(all9mins)
+console.log(allEditButtons)
+console.log(cardImg)
 const firstButton = document.getElementById('load-1')
 const secondButton = document.getElementById('load-2')
 
-const replaceImgs = (photos) => {
-  const arrayImgs = photos.photos
-  console.log(arrayImgs)
-  arrayImgs.forEach((photo) => {
-    cardImg = photo.src.medium
-  })
-}
-
-const getImgs = () => {
-  fetch(firstURL, {
+// allEditButtons.forEach((button) => {
+//   button.innerText = 'Hide'
+// })
+const getImgs = (query) => {
+  fetch('https://api.pexels.com/v1/search?query=' + query, {
     headers: {
       authorization: '3gAvQUa830BcITMAJ0NXzwxqQlXmCmw1RK8Q8LAy6bPviMmY5r31UqIU',
     },
@@ -27,12 +29,35 @@ const getImgs = () => {
       }
     })
     .then((data) => {
-      console.log(data)
-      replaceImgs(data)
+      cardImg.forEach((card, i) => {
+        card.src = data.photos[i].src.medium
+      })
+
+      allEditButtons.forEach((button) => {
+        button.innerText = 'Hide'
+        button.addEventListener('click', (event) => {
+          event.target.closest('.col-md-4').remove()
+        })
+      })
+      all9mins.forEach((label, i) => {
+        label.innerText = data.photos[i].id
+      })
+
+      console.log('DATA', data)
     })
     .catch((err) => {
       console.log(err)
     })
 }
 
-firstButton.addEventListener('click', getImgs)
+firstButton.addEventListener('click', () => {
+  getImgs('moon')
+})
+
+secondButton.addEventListener('click', () => {
+  getImgs('wolfs')
+})
+
+buttonField.addEventListener('click', () => {
+  getImgs(inputField.value)
+})
